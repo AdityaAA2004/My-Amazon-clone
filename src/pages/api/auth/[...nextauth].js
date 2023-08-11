5d6bd0ab4e99896205b47882b20e7081732e256c
AdityaAA2004 <116763944+AdityaAA2004@users.noreply.github.com>
2023-08-11 12:38:08 -0400

    Update [...nextauth].js --experimental

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -15,55 +15,55 @@ const options = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     // Add other providers here if needed
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        try {
-          const { username, password } = credentials;
-          const auth = getAuth(app)
+    // CredentialsProvider({
+    //   // The name to display on the sign in form (e.g. 'Sign in with...')
+    //   name: 'Credentials',
+    //   // The credentials is used to generate a suitable form on the sign in page.
+    //   // You can specify whatever fields you are expecting to be submitted.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   // You can pass any HTML attribute to the <input> tag through the object.
+    //   credentials: {
+    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
+    //     password: { label: "Password", type: "password" }
+    //   },
+    //   async authorize(credentials, req) {
+    //     try {
+    //       const { username, password } = credentials;
+    //       const auth = getAuth(app)
           
-          // Check if the user exists in Firestore
-          const userDocRef = doc(db, 'users', username);
-          const userSnapshot = await getDoc(userDocRef);
-          const userData = userSnapshot.data();
+    //       // Check if the user exists in Firestore
+    //       const userDocRef = doc(db, 'users', username);
+    //       const userSnapshot = await getDoc(userDocRef);
+    //       const userData = userSnapshot.data();
 
-          if (userData) {
-            // User exists, attempt to sign in
-            await signInWithEmailAndPassword(auth, userData.email, password);
-            return userData; // Return the user data if sign-in is successful
-          } else {
-            // User doesn't exist, create a new user
-            const { user } = await createUserWithEmailAndPassword(auth, username, password);
+    //       if (userData) {
+    //         // User exists, attempt to sign in
+    //         await signInWithEmailAndPassword(auth, userData.email, password);
+    //         return userData; // Return the user data if sign-in is successful
+    //       } else {
+    //         // User doesn't exist, create a new user
+    //         const { user } = await createUserWithEmailAndPassword(auth, username, password);
 
-            // Store user data in Firestore
-            await setDoc(userDocRef, {
-              email: user.email
-            });
+    //         // Store user data in Firestore
+    //         await setDoc(userDocRef, {
+    //           email: user.email
+    //         });
 
-            return { email: user.email }; // Return user data
-          }
-        } catch (error) {
-          // Handle authentication errors
-          console.error("Authentication error:", error);
-          return null;
-        }
-      }
+    //         return { email: user.email }; // Return user data
+    //       }
+    //     } catch (error) {
+    //       // Handle authentication errors
+    //       console.error("Authentication error:", error);
+    //       return null;
+    //     }
+    //   }
     
-    })
+    // })
     
   ],
 
   secret: process.env.NEXTAUTH_SECRET,
-  adapter:FirestoreAdapter({db:db, ...firestoreFunctions})
+  // adapter:FirestoreAdapter({db:db, ...firestoreFunctions})
 };
 
 export default NextAuth(options);
