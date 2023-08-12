6b5b641662df7ce8b3d4d242201edc997c25b906
AdityaAA2004 <anandad1@msu.edu>
2023-08-12 06:12:46 -0400

    Experimental

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
+import sessionReducer from '../slices/sessionSlice'
 //this is the gloabl store.
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
+    session: sessionReducer
   },
 });
