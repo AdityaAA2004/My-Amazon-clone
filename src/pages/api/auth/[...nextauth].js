3461ea588f07e4fa222867a0513f337d84b2d953
AdityaAA2004 <anandad1@msu.edu>
2023-08-12 06:51:20 -0400

    More experimental changes

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { useDispatch } from "react-redux";
 // import CredentialsProvider from "next-auth/providers/credentials"
 // import { FirestoreAdapter } from "@auth/firebase-adapter";
 // import { app, db } from "../../../../firebase";
@@ -14,56 +15,24 @@ const options = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // Add other providers here if needed
-    // CredentialsProvider({
-    //   // The name to display on the sign in form (e.g. 'Sign in with...')
-    //   name: 'Credentials',
-    //   // The credentials is used to generate a suitable form on the sign in page.
-    //   // You can specify whatever fields you are expecting to be submitted.
-    //   // e.g. domain, username, password, 2FA token, etc.
-    //   // You can pass any HTML attribute to the <input> tag through the object.
-    //   credentials: {
-    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
-    //     password: { label: "Password", type: "password" }
-    //   },
-    //   async authorize(credentials, req) {
-    //     try {
-    //       const { username, password } = credentials;
-    //       const auth = getAuth(app)
-          
-    //       // Check if the user exists in Firestore
-    //       const userDocRef = doc(db, 'users', username);
-    //       const userSnapshot = await getDoc(userDocRef);
-    //       const userData = userSnapshot.data();
-
-    //       if (userData) {
-    //         // User exists, attempt to sign in
-    //         await signInWithEmailAndPassword(auth, userData.email, password);
-    //         return userData; // Return the user data if sign-in is successful
-    //       } else {
-    //         // User doesn't exist, create a new user
-    //         const { user } = await createUserWithEmailAndPassword(auth, username, password);
-
-    //         // Store user data in Firestore
-    //         await setDoc(userDocRef, {
-    //           email: user.email
-    //         });
-
-    //         return { email: user.email }; // Return user data
-    //       }
-    //     } catch (error) {
-    //       // Handle authentication errors
-    //       console.error("Authentication error:", error);
-    //       return null;
-    //     }
-    //   }
-    
-    // })
     
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      // Handle sign-in logic here...
+      const dispatch = useDispatch();
+      dispatch(setUser(user)); // Update the user in the session state
+      return true;
+    },
+    async signOut(user, account) {
+      // Handle sign-out logic here...
+      const dispatch = useDispatch();
+      dispatch(clearUser()); // Clear the user from the session state
+      return true;
+    },
+  },
 
   secret: process.env.NEXTAUTH_SECRET,
-  // adapter:FirestoreAdapter({db:db, ...firestoreFunctions})
 };
 
 export default NextAuth(options);
