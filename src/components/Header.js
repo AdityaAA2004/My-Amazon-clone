e17289c7539886480eebb2df5c44ba7b0fe7983d
AdityaAA2004 <116763944+AdityaAA2004@users.noreply.github.com>
2023-08-12 05:43:34 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,7 +45,7 @@ function Header() {
                 {/* text-xs is abuot the extreme small size of the text. mx-6 is about margin in x-axis of 6 px */}
                 {/* 'whitespace-nowrap' means the flex items will not wrap over one another on descreasing the size of the screen. */}
                 <div onClick={!session ? signIn: signOut} className='link'>
-                    <p>{session ? `Hello ${session.name}`: "Sign In"}</p>
+                    <p>{session ? `Hello ${session.user.name}`: "Sign In"}</p>
                     <p className='font-extrabold md:text-sm' >Account and Lists</p>
                 </div>
                 <div className='link'>
