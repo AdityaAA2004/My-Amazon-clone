383dd13f73f1d3e287464c0c879eba0834a5b033
AdityaAA2004 <116763944+AdityaAA2004@users.noreply.github.com>
2023-08-12 10:02:53 -0400

    Update store.js for persisting redux state

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import basketReducer from "../slices/basketSlice";
-import sessionReducer from '../slices/sessionSlice'
-//this is the gloabl store.
+import basketReducer from '../slices/basketSlice'
+import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  sessionStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, basketReducer)
+
 export const store = configureStore({
-  reducer: {
-    basket: basketReducer,
-    session: sessionReducer
-  },
-});
+  reducer: persistedReducer,
+  middleware: [thunk]
+})
+
+export const persistor = persistStore(store)
