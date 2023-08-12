4ffbc208a4c944a09c9eb7a05922df85f4d06b56
AdityaAA2004 <anandad1@msu.edu>
2023-08-12 07:18:51 -0400

    More experimental changes 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,8 +44,8 @@ function Header() {
                 {/* 'space-x-6' allows the flex items to give a space between these flex items. Here, there is a horizontal space of 6px between each flex-item*/}
                 {/* text-xs is abuot the extreme small size of the text. mx-6 is about margin in x-axis of 6 px */}
                 {/* 'whitespace-nowrap' means the flex items will not wrap over one another on descreasing the size of the screen. */}
-                <div onClick={!session ? signIn: signOut} className='link'>
-                    <p>{session ? `Hello ${session.name}`: "Sign In"}</p>
+                <div onClick={session.user === null ? signIn: signOut} className='link'>
+                    <p>{session.user !==null ? `Hello ${session.name}`: "Sign In"}</p>
                     <p className='font-extrabold md:text-sm' >Account and Lists</p>
                 </div>
                 <div className='link'>
