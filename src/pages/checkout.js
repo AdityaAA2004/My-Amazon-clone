2da4703c22702f74578a7fd76a20d04a544f8b18
AdityaAA2004 <anandad1@msu.edu>
2023-08-14 11:29:27 -0400

    Another quantity logic

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -19,18 +19,11 @@ function checkout() {
     console.log("Checkout session created");
     //Call the backend to create a checkout session
     const stripe = await stripePromise;
-    const basketforCheckout= []
-    items.map(item=>{
-      idToPush = item.id
-      var itemToPush = items.find((idToPush)=> item.id == idToPush)
-      if (!basketforCheckout.includes(itemToPush)){
-        basketforCheckout.push(itemToPush)
-      }
-    })
+
     console.log(stripe)
     const checkoutSession = await axios.post('api/create-checkout-session',
     {
-      items: basketforCheckout,
+      items: newBasket,
       email: session?.data?.user?.email
     })
   }
