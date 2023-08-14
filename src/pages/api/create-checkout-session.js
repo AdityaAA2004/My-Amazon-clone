c2bf2b8ee28f7545d2e3e26c791bb6ef80de87b4
AdityaAA2004 <anandad1@msu.edu>
2023-08-14 09:45:06 -0400

    Small fix - 2

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require('stripe')
 export default async (req,res) => {
     const {items,email} = req.body
     console.log(items)
