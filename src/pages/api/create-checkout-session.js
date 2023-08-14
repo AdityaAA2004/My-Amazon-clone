77d123e36b55576c79fbde21dedf1768a1d97838
AdityaAA2004 <anandad1@msu.edu>
2023-08-14 11:47:06 -0400

    Bug fix 3

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -2,6 +2,5 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 export default async (req,res) => {
     const {items,email} = req.body
     console.log(items)
-    console.log(itemsBasket.filter(item=> item.id === items[0].id))
     console.log(email)
 }
