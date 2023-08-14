bd376a85c5cc5d23042312b204591a01ae8e15a6
AdityaAA2004 <anandad1@msu.edu>
2023-08-14 11:25:45 -0400

    Merge conflict

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-const stripe = require('stripe')
-=======
 import { useSelector } from 'react-redux'
 import { selectItems } from '../../slices/basketSlice'
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
->>>>>>> 12a69688d6d251330815b827e4bd2d496b4e0a46
 export default async (req,res) => {
     const itemsBasket= useSelector(selectItems)
     const {items,email} = req.body
