1a74f2572b6b088d01994dc766798ad350542124
AdityaAA2004 <anandad1@msu.edu>
2023-08-15 11:09:00 -0400

    Success page editted

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,8 +1,11 @@
+import Header from "../components/Header";
+
 export default function success(){
     return (
         <div className=" bg-gray-100">
-            <div className=" flex">
-                <p>Success</p>
+            <Header />
+            <div className='flex flex-col m-5 bg-white z-40 p-10'>
+                <p className='top-2 right-2 text-xs italic text-gray-400'>success</p>
             </div>
         </div>
     )
