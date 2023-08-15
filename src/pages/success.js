e9acb52490690d391343f9c31ca8795439ae93e1
AdityaAA2004 <anandad1@msu.edu>
2023-08-15 11:12:06 -0400

    Success page editted

--- src/pages/success.js
+++ src/pages/success.js
@@ -5,7 +5,7 @@ export default function success(){
         <div className=" bg-gray-100">
             <Header />
             <div className='flex flex-col m-5 bg-white z-40 p-10'>
-                <p className='top-2 right-2 text-xs italic text-gray-400'>success</p>
+                <p className='top-2 right-2 text-xl italic text-gray-400'>success</p>
             </div>
         </div>
     )
