f2d25f26a55ced2dabe66f1c25bca0727ba66536
AdityaAA2004 <anandad1@msu.edu>
2023-08-15 11:05:36 -0400

    Success page added

---
+++ src/pages/success.js
@@ -0,0 +1,9 @@
+export default function success(){
+    return (
+        <div className=" bg-gray-100">
+            <div className=" flex">
+                <p>Success</p>
+            </div>
+        </div>
+    )
+}
